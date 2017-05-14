4b01820ff3aa33bc7bccda20e1e1430eb6383ecc
yangjiu <yeung@yangjiudeMacBook-Pro.local>
2017-05-14 23:06:11 +0800

    add build gn shell

---
+++ build-gn.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+set -v
+
+# Get the sources
+mkdir gn-standalone
+cd gn-standalone
+mkdir tools
+cd tools
+git clone https://chromium.googlesource.com/chromium/src/tools/gn
+cd ..
+mkdir -p third_party/libevent
+cd third_party/libevent
+curl -O https://chromium.googlesource.com/chromium/chromium/+archive/master/third_party/libevent.tar.gz
+tar -xvzf libevent.tar.gz
+#for mac machine
+cd ..
+git clone https://github.com/quentum/apple_apsl.git
+cd ..
+git clone https://chromium.googlesource.com/chromium/src/base
+git clone https://chromium.googlesource.com/chromium/src/build
+git clone https://chromium.googlesource.com/chromium/src/build/config
+mkdir testing
+cd testing
+git clone https://chromium.googlesource.com/chromium/testing/gtest
+cd ..
+
+# Build
+cd tools/gn
+./bootstrap/bootstrap.py -s
+
+# At this point, the resulting binary is at:
+# gn-standalone/out/Release/gn
